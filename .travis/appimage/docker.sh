228332e3ec64879b248edceae8bf47387f46c614
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-08-27 18:38:06 -0700

    Update docker.sh

--- .travis/appimage/docker.sh
+++ .travis/appimage/docker.sh
@@ -9,27 +9,27 @@ export LD_LIBRARY_PATH=$QT_BASE_DIR/lib/x86_64-linux-gnu:$QT_BASE_DIR/lib:$LD_LI
 export PKG_CONFIG_PATH=$QT_BASE_DIR/lib/pkgconfig:$PKG_CONFIG_PATH
 
 ln -s /home/dolphin/.conan /root
-ls -al
+
 cd /pcsx2
-ls -al
+
 mkdir appimage && cd appimage
 git clone https://github.com/beaumanvienna/pcsx2.git
 git submodule update --init --recursive
-ls -al
+
 cd pcsx2/
-ls -al
+
 
 mkdir build
 cd build
 cmake .. -G Ninja -DLINUX_LOCAL_DEV=true -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++
 ninja
 
-cat /pcsx2/appimage/pcsx2/build/CMakeFiles/CMakeError.log | curl -F 'f:1=<-' ix.io
+#cat /pcsx2/appimage/pcsx2/build/CMakeFiles/CMakeError.log | curl -F 'f:1=<-' ix.io
 
 cd /tmp
 curl -sLO "https://raw.githubusercontent.com/qurious-pixel/pcsx2/$branch/.travis/appimage/appimage.sh"
-#chmod a+x appimage.sh
-#./appimage.sh
-ls -al /pcsx2
-ls -al /pcsx2/appimage/pcsx2/build
-#ls -al /dolphin/build/Binaries
+chmod a+x appimage.sh
+./appimage.sh
+#ls -al /pcsx2
+#ls -al /pcsx2/appimage/pcsx2/build
+ls -al /pcsx2/appimage/pcsx2/build/bin
