eebd1344445fb9fb70f678e282ad09a1bf3b07c4
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-08-28 20:51:16 -0700

    Update docker.sh

--- .travis/appimage/docker.sh
+++ .travis/appimage/docker.sh
@@ -13,7 +13,7 @@ ln -s /home/pcsx2/.conan /root
 cd /pcsx2
 
 mkdir appimage && cd appimage
-git clone https://github.com/beaumanvienna/pcsx2.git
+git clone --branch x86_64-support https://github.com/beaumanvienna/pcsx2.git
 git submodule update --init --recursive
 
 cd pcsx2/
