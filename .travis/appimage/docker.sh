b395e4920952c11a202c4379bac5eabed9611ea0
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-09-01 13:22:20 -0700

    Update docker.sh

--- .travis/appimage/docker.sh
+++ .travis/appimage/docker.sh
@@ -8,9 +8,6 @@ export PATH=$QT_BASE_DIR/bin:$PATH
 export LD_LIBRARY_PATH=$QT_BASE_DIR/lib/x86_64-linux-gnu:$QT_BASE_DIR/lib:$LD_LIBRARY_PATH
 export PKG_CONFIG_PATH=$QT_BASE_DIR/lib/pkgconfig:$PKG_CONFIG_PATH
 
-apt-get update
-apt-get install -y libgtk2.0-0
-
 ln -s /home/pcsx2/.conan /root
 
 cd /pcsx2
@@ -26,9 +23,8 @@ git submodule update --init --recursive
 
 mkdir build
 cd build
-cmake .. -G Ninja -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++ -DCMAKE_BUILD_TYPE=Release -DGTK3_API=TRUE 
--DPACKAGE_MODE=TRUE -DXDG_STD=TRUE -DPLUGIN_DIR="./usr/bin/pcsx2/"
-#\ -DDISABLE_ADVANCE_SIMD=TRUE -DGSDX_LEGACY=TRUE
+cmake .. -G Ninja -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++ -DCMAKE_BUILD_TYPE=Release -DPACKAGE_MODE=TRUE -DXDG_STD=TRUE -DPLUGIN_DIR="./usr/bin/pcsx2/"
+#\ -DDISABLE_ADVANCE_SIMD=TRUE -DGSDX_LEGACY=TRUE -DGTK3_API=TRUE 
 ninja
 
 #cat /pcsx2/appimage/pcsx2/build/CMakeFiles/CMakeError.log | curl -F 'f:1=<-' ix.io
