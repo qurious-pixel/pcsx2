15dd9591ca92f7913ea9c5ed489c0b6c8a0fa674
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-09-02 13:05:43 -0700

    Update docker.sh

--- .travis/appimage/docker.sh
+++ .travis/appimage/docker.sh
@@ -8,6 +8,9 @@ export PATH=$QT_BASE_DIR/bin:$PATH
 export LD_LIBRARY_PATH=$QT_BASE_DIR/lib/x86_64-linux-gnu:$QT_BASE_DIR/lib:$LD_LIBRARY_PATH
 export PKG_CONFIG_PATH=$QT_BASE_DIR/lib/pkgconfig:$PKG_CONFIG_PATH
 
+apt-get update
+apt-get install -y libsoundtouch1 
+
 ln -s /home/pcsx2/.conan /root
 
 cd /pcsx2
