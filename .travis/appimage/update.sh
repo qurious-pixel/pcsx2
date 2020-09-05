7f9b7640b269717b01147461f139c39c55312b47
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-09-05 06:54:23 -0700

    Update update.sh

--- .travis/appimage/update.sh
+++ .travis/appimage/update.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-$APPDIR/usr/bin/zenity --question --timeout=10 --title="PCSX2 updater" --text="New update available. Update now?" --icon-name=PCSX2 --window-icon=PCSX2.svg --height=80 --width=400
+if [ -f /usr/bin/zenity ]; then
+	zenity --question --timeout=10 --title="PCSX2 updater" --text="New update available. Update now?" --icon-name=PCSX2 --window-icon=PCSX2.svg --height=80 --width=400
+else
+	dialog --title PCSX2 --timeout 10 --yesno "New update available. Update now?" 0 0
+fi
+	
 answer=$?
 
 if [ "$answer" -eq 0 ]; then 
@@ -8,7 +13,7 @@ if [ "$answer" -eq 0 ]; then
 	$APPDIR/usr/bin/AppImageUpdate $PWD/PCSX2-x86_64.AppImage && $PWD/PCSX2-x86_64.AppImage
 elif [ "$answer" -eq 1 ]; then
 	$APPDIR/AppRun-patched
-elif [ "$answer" -eq 5 ]; then
+else 
 	$APPDIR/AppRun-patched
 fi
 exit 0
