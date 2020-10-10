3fbc86ef7e2598c251f4c737a09ec874c8d9282e
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-10-09 20:01:10 -0700

    Update update.sh
    fix updater

--- .travis/appimage/update.sh
+++ .travis/appimage/update.sh
@@ -3,12 +3,14 @@
 unset LD_LIBRARY_PATH
 if [ -f /usr/bin/zenity ]; then
 	zenity --question --timeout=10 --title="PCSX2 updater" --text="New update available. Update now?" --icon-name=pcsx2 --window-icon=pcsx2.svg --height=80 --width=400
+	answer=$?
 else
 	dialog --title PCSX2 --timeout 10 --yesno "New update available. Update now?" 0 0
+	answer=$?
 fi
 
 export LD_LIBRARY_PATH="/tmp/PCSX2LIBS:$APPDIR/usb/lib:$LD_LIBRARY_PATH"
-answer=$?
+
 
 if [ "$answer" -eq 0 ]; then 
 	export LD_PRELOAD="$APPDIR/usr/lib/updater/libcurl.so.4"
