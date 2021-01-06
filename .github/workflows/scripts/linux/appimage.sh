145b5382ffaa5cf3cc295b5f7bf3f064e990e7a9
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2021-01-05 17:53:55 -0800

    Update appimage.sh

--- .github/workflows/scripts/linux/appimage.sh
+++ .github/workflows/scripts/linux/appimage.sh
@@ -57,7 +57,7 @@ export PATH=$(readlink -f /tmp/squashfs-root/usr/bin/):$PATH
 	cp /usr/lib/x86_64-linux-gnu/libSDL2-2.0.so.0 $GITHUB_WORKSPACE/squashfs-root/usr/lib/
 	cp /usr/lib/x86_64-linux-gnu/libsndio.so.6.1 $GITHUB_WORKSPACE/squashfs-root/usr/lib/
 mkdir -p $GITHUB_WORKSPACE/squashfs-root/usr/lib/plugins
-find $BUILDBIN/../plugins -iname '*.so' -exec cp {} $GITHUB_WORKSPACE/squashfs-root/usr/lib/plugins \;
+find $BUILDBIN/plugins -iname '*.so' -exec cp {} $GITHUB_WORKSPACE/squashfs-root/usr/lib/plugins \;
 arr=( $(ls -d $GITHUB_WORKSPACE/squashfs-root/usr/lib/plugins/* ) )
 for i in "${arr[@]}"; do patchelf --set-rpath /tmp/PCSX2LIBS "$i"; done
 patchelf --set-rpath /tmp/PCSX2 $GITHUB_WORKSPACE/squashfs-root/usr/lib/libSDL2-2.0.so.0
