ff635f04ecf2a3a000de140a7823f6d7e4a815f4
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2021-02-11 13:28:59 -0800

    Update docker.sh
    GameIndex.yaml at default

--- .github/workflows/scripts/linux/docker.sh
+++ .github/workflows/scripts/linux/docker.sh
@@ -7,7 +7,17 @@ realpath . && ls -al .
 
 mkdir build
 cd build
-cmake .. -G Ninja -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++ -DCMAKE_BUILD_TYPE=Release -DPACKAGE_MODE=TRUE -DGTK3_API=TRUE -DXDG_STD=TRUE -DDISABLE_ADVANCE_SIMD=TRUE -DCMAKE_INSTALL_LIBDIR="/tmp/" -DCMAKE_INSTALL_DATADIR="/tmp/"
+cmake .. 
+    -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc      \
+    -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++    \
+    -DCMAKE_BUILD_TYPE=Release                  \
+    -DPACKAGE_MODE=TRUE                         \      
+    -DGTK3_API=TRUE                             \
+    -DXDG_STD=TRUE                              \
+    -DDISABLE_ADVANCE_SIMD=TRUE                 \
+    -DCMAKE_INSTALL_LIBDIR="/tmp/"              \          
+    #-DCMAKE_INSTALL_DATADIR="/tmp/"            \
+    -G Ninja
 ninja
 
 
