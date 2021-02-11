c6ed0b313c40e969154329f758f685533162dba7
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2021-02-11 14:41:04 -0800

    Update docker.sh

--- .github/workflows/scripts/linux/docker.sh
+++ .github/workflows/scripts/linux/docker.sh
@@ -5,19 +5,19 @@ branch=`echo ${GITHUB_REF##*/}`
 cd /pcsx2
 realpath . && ls -al .
 
-mkdir build
-cd build
+mkdir build && cd build || exit 1
 
 cmake ..                                        \
     -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc      \
     -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++    \
     -DCMAKE_BUILD_TYPE=Release                  \
     -DPACKAGE_MODE=TRUE                         \      
-    -DGTK3_API=TRUE                             \
     -DXDG_STD=TRUE                              \
     -DDISABLE_ADVANCE_SIMD=TRUE                 \
     -DCMAKE_INSTALL_LIBDIR="/tmp/"              \          
     -G Ninja
+    
+    #-DGTK3_API=TRUE                            \
     #-DCMAKE_INSTALL_DATADIR="/tmp/"            \
 
 ninja
