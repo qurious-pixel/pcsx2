02160d9053779cd56414f528a63b2f7051f9e313
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2021-02-11 06:19:24 -0800

    Create docker.sh
    docker script for build

---
+++ .github/workflows/scripts/linux/docker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -ex
+
+branch=`echo ${GITHUB_REF##*/}`
+
+cd /pcsx2
+
+mkdir build
+cd build
+cmake .. -G Ninja -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++ -DCMAKE_BUILD_TYPE=Release -DPACKAGE_MODE=TRUE -DGTK2_API=TRUE -DXDG_STD=TRUE -DDISABLE_ADVANCE_SIMD=TRUE -DCMAKE_INSTALL_LIBDIR="/tmp/" -DCMAKE_INSTALL_DATADIR="/tmp/"
+ninja
+
+
+cp ../.github/workflows/scripts/linux/appimage.sh /tmp
+cd /tmp
+#chmod a+x appimage.sh
+./appimage.sh
