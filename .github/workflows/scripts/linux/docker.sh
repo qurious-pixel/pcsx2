308ee89bb427b4fe0270f8a1975b12a935f3d008
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2021-04-23 10:19:58 -0700

    Update docker.sh
    add build po false

--- .github/workflows/scripts/linux/docker.sh
+++ .github/workflows/scripts/linux/docker.sh
@@ -11,6 +11,7 @@ cmake ..                                        \
     -DPACKAGE_MODE=TRUE                         \
     -DXDG_STD=TRUE                              \
     -DDISABLE_ADVANCE_SIMD=TRUE                 \
+    -DCMAKE_BUILD_PO=FALSE                      \
     -DCMAKE_INSTALL_LIBDIR="/tmp/"              \
     -DCMAKE_INSTALL_DATADIR="/tmp/"             \
     -G Ninja
