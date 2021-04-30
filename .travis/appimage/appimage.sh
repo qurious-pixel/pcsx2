78647cf84fb2cd1f10c8ee500d872682a2e46fd0
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2021-04-29 23:23:09 -0700

    Update appimage.sh

--- .travis/appimage/appimage.sh
+++ .travis/appimage/appimage.sh
@@ -14,7 +14,8 @@ cd /tmp
 ./linuxdeployqt-continuous-x86_64.AppImage --appimage-extract
 cd $HOME
 mkdir -p squashfs-root/usr/bin
-ls -al $BUILDBIN/../
+#ls -al $BUILDBIN/
+ls -al /pcsx2/appimage/pcsx2/build/
 cp -P "$BUILDBIN"/PCSX2 $HOME/squashfs-root/usr/bin/
 patchelf --set-rpath /tmp/PCSX2LIBS $HOME/squashfs-root/usr/bin/PCSX2
 
