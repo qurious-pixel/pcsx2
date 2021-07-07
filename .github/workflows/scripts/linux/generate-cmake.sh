0afd5117db7d4fece807d1c86ac5b54d5cbd04f1
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2021-07-07 16:10:51 -0700

    fix directory traversal

--- .github/workflows/scripts/linux/generate-cmake.sh
+++ .github/workflows/scripts/linux/generate-cmake.sh
@@ -13,15 +13,18 @@ fi
 if [ ${PLATFORM} = x86 ]; then
     ADDITIONAL_CMAKE_ARGS="$ADDITIONAL_CMAKE_ARGS -DCMAKE_TOOLCHAIN_FILE=cmake/linux-compiler-i386-multilib.cmake"
 fi
-          cmake ..                                    \
-          -DCMAKE_CXX_COMPILER_LAUNCHER=ccache        \
-          -DCMAKE_BUILD_TYPE=Release                  \
-          -DPACKAGE_MODE=TRUE                         \
-          -DDISABLE_ADVANCE_SIMD=TRUE                 \
-          -DCMAKE_INSTALL_LIBDIR="/tmp/"              \
-          -DCMAKE_INSTALL_DATADIR="/tmp/"             \
-          -DCMAKE_INSTALL_DOCDIR="/tmp/PCSX2"         \
-          -DOpenGL_GL_PREFERENCE="LEGACY"             \
-          -DOPENGL_opengl_LIBRARY=""                  \
-          -DXDG_STD=TRUE ${ADDITIONAL_CMAKE_ARGS}     \
-          -B build
+echo "Additional CMake Args - ${ADDITIONAL_CMAKE_ARGS}"
+
+# Generate CMake into ./build
+cmake                                       \
+-DCMAKE_CXX_COMPILER_LAUNCHER=ccache        \
+-DCMAKE_BUILD_TYPE=Release                  \
+-DPACKAGE_MODE=TRUE                         \
+-DDISABLE_ADVANCE_SIMD=TRUE                 \
+-DCMAKE_INSTALL_LIBDIR="/tmp/"              \
+-DCMAKE_INSTALL_DATADIR="/tmp/"             \
+-DCMAKE_INSTALL_DOCDIR="/tmp/PCSX2"         \
+-DOpenGL_GL_PREFERENCE="LEGACY"             \
+-DOPENGL_opengl_LIBRARY=""                  \
+-DXDG_STD=TRUE ${ADDITIONAL_CMAKE_ARGS}     \
+-B build
