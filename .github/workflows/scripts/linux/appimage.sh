b971f61e187f24ff249f31a57ab52349b8311fbe
lubuntu <qurious@gmx.com>
2021-07-07 18:16:33 -0700

    CI: merge linux and appimage workflows

--- .github/workflows/scripts/linux/appimage.sh
+++ .github/workflows/scripts/linux/appimage.sh
@@ -1,31 +1,24 @@
-#!/bin/bash 
-
-set -ex
-
 echo "${PLATFORM}"
 if [ "${PLATFORM}" == "x86" ]; then
-	APPARCH="i686"
-	ARCH="i386"
+  APPARCH="i686"
+  ARCH="i386"
 else
-	APPARCH="x86_64"
-	ARCH="x86_64"
+  APPARCH="x86_64"
+  ARCH="x86_64"
 fi
-
-BUILDPATH=/pcsx2/build
+BUILDPATH=$GITHUB_WORKSPACE/build
 BUILDBIN=$BUILDPATH/pcsx2
-
 cd /tmp
-	curl -sSfLO "https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-$ARCH.AppImage"
-	chmod a+x linuxdeploy*.AppImage
+curl -sSfLO "https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-$ARCH.AppImage"
+chmod a+x linuxdeploy*.AppImage
 ./linuxdeploy-$ARCH.AppImage --appimage-extract
-	mv /tmp/squashfs-root/usr/bin/patchelf /tmp/squashfs-root/usr/bin/patchelf.orig
-	cp /usr/local/bin/patchelf /tmp/squashfs-root/usr/bin/patchelf
-cd /pcsx2
+mv /tmp/squashfs-root/usr/bin/patchelf /tmp/squashfs-root/usr/bin/patchelf.orig
+sudo cp /usr/local/bin/patchelf /tmp/squashfs-root/usr/bin/patchelf
+cd $GITHUB_WORKSPACE
 mkdir -p squashfs-root/usr/bin
 ls -al $BUILDBIN
-cp -P "$BUILDBIN"/PCSX2 /pcsx2/squashfs-root/usr/bin/
-patchelf --set-rpath /tmp/PCSX2 /pcsx2/squashfs-root/usr/bin/PCSX2
-
+cp -P "$BUILDBIN"/PCSX2 $GITHUB_WORKSPACE/squashfs-root/usr/bin/
+patchelf --set-rpath /tmp/PCSX2 $GITHUB_WORKSPACE/squashfs-root/usr/bin/PCSX2
 cp ./pcsx2/gui/Resources/AppIcon64.png ./squashfs-root/PCSX2.png
 cp ./linux_various/PCSX2.desktop.in ./squashfs-root/PCSX2.desktop 
 sed -i -e 's|Categories=@PCSX2_MENU_CATEGORIES@|Categories=Game;Emulator;|g' ./squashfs-root/PCSX2.desktop
@@ -36,30 +29,26 @@ mkdir -p squashfs-root/usr/share/icons && cp ./squashfs-root/PCSX2.png ./squashf
 mkdir -p squashfs-root/usr/share/icons/hicolor/scalable/apps && cp ./squashfs-root/PCSX2.png ./squashfs-root/usr/share/icons/hicolor/scalable/apps
 mkdir -p squashfs-root/usr/share/pixmaps && cp ./squashfs-root/PCSX2.png ./squashfs-root/usr/share/pixmaps
 mkdir -p squashfs-root/usr/lib/
-cp ./.github/workflows/scripts/linux/AppRun /pcsx2/squashfs-root/AppRun
-curl -sSfL "https://github.com/AppImage/AppImageKit/releases/download/continuous/AppRun-$APPARCH" -o /pcsx2/squashfs-root/AppRun-patched
+cp ./.github/workflows/scripts/linux/AppRun $GITHUB_WORKSPACE/squashfs-root/AppRun
+curl -sSfL "https://github.com/AppImage/AppImageKit/releases/download/continuous/AppRun-$APPARCH" -o $GITHUB_WORKSPACE/squashfs-root/AppRun-patched
 chmod a+x ./squashfs-root/AppRun
 chmod a+x ./squashfs-root/runtime
 chmod a+x ./squashfs-root/AppRun-patched
-
-echo "$name" > /pcsx2/squashfs-root/version.txt
-
-mkdir -p /pcsx2/squashfs-root/usr/bin/{plugins,vm}
-cp -r /pcsx2/bin/Langs /pcsx2/squashfs-root/usr/bin/
-cp /pcsx2/bin/docs/{Configuration_Guide.pdf,PCSX2_FAQ.pdf} /pcsx2/squashfs-root/usr/bin/plugins
-cp /pcsx2/bin/cheats_ws.zip /pcsx2/squashfs-root/usr/bin/plugins
+echo "$name" > $GITHUB_WORKSPACE/squashfs-root/version.txt
+mkdir -p $GITHUB_WORKSPACE/squashfs-root/usr/bin/{plugins,vm}
+cp -r $GITHUB_WORKSPACE/bin/Langs $GITHUB_WORKSPACE/squashfs-root/usr/bin/
+cp $GITHUB_WORKSPACE/bin/docs/{Configuration_Guide.pdf,PCSX2_FAQ.pdf} $GITHUB_WORKSPACE/squashfs-root/usr/bin/plugins
+cp $GITHUB_WORKSPACE/bin/cheats_ws.zip $GITHUB_WORKSPACE/squashfs-root/usr/bin/plugins
 if [[ -e "$BUILDPATH/plugins" ]]; then
-	find "$BUILDPATH/plugins" -iname '*.so' -exec cp {} /pcsx2/squashfs-root/usr/bin/plugins \;
+  find "$BUILDPATH/plugins" -iname '*.so' -exec cp {} $GITHUB_WORKSPACE/squashfs-root/usr/bin/plugins \;
 fi
-cp ./bin/GameIndex.yaml /pcsx2/squashfs-root/usr/bin/plugins/GameIndex.yaml
+cp ./bin/GameIndex.yaml $GITHUB_WORKSPACE/squashfs-root/usr/bin/plugins/GameIndex.yaml
 export UPD_INFO="gh-releases-zsync|PCSX2|pcsx2|latest|$name.AppImage.zsync"
 export OUTPUT=$name.AppImage
-/tmp/squashfs-root/AppRun --appdir=/pcsx2/squashfs-root/ -d /pcsx2/squashfs-root/PCSX2.desktop -i /pcsx2/squashfs-root/PCSX2.png --output appimage
-
-mkdir -p /pcsx2/artifacts/
+/tmp/squashfs-root/AppRun --appdir=$GITHUB_WORKSPACE/squashfs-root/ -d $GITHUB_WORKSPACE/squashfs-root/PCSX2.desktop -i $GITHUB_WORKSPACE/squashfs-root/PCSX2.png --output appimage
+mkdir -p $GITHUB_WORKSPACE/artifacts/
 ls -al .
-mv "$name.AppImage" /pcsx2/artifacts # && mv "$name.AppImage.zsync" /pcsx2/artifacts
+mv "$name.AppImage" $GITHUB_WORKSPACE/artifacts # && mv "$name.AppImage.zsync" $GITHUB_WORKSPACE/artifacts
 chmod -R 777 ./artifacts
 cd ./artifacts
 ls -al .
-
