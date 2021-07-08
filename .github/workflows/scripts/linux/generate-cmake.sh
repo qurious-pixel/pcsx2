1809eaba02d7c44e4c23587a7c61463e119c2dff
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2021-07-07 21:53:28 -0700

    double quotes

--- .github/workflows/scripts/linux/generate-cmake.sh
+++ .github/workflows/scripts/linux/generate-cmake.sh
@@ -10,7 +10,7 @@ else
   export CXX=clang++
 fi
 
-if [ ${PLATFORM} = x86 ]; then
+if [ "${PLATFORM}" = x86 ]; then
     ADDITIONAL_CMAKE_ARGS="$ADDITIONAL_CMAKE_ARGS -DCMAKE_TOOLCHAIN_FILE=cmake/linux-compiler-i386-multilib.cmake"
 fi
 echo "Additional CMake Args - ${ADDITIONAL_CMAKE_ARGS}"
@@ -26,6 +26,6 @@ cmake                                       \
 -DCMAKE_INSTALL_DOCDIR="/tmp/PCSX2"         \
 -DOpenGL_GL_PREFERENCE="LEGACY"             \
 -DOPENGL_opengl_LIBRARY=""                  \
--DXDG_STD=TRUE ${ADDITIONAL_CMAKE_ARGS}     \
+-DXDG_STD=TRUE "${ADDITIONAL_CMAKE_ARGS}"     \
 -GNinja                                     \
 -B build
